493f5056abbf66b1dedf4f155fec5c58d14e5389
DESKTOP-HTOPP7T\Kevin <kevinfachreza@yahoo.com>
2016-11-04 16:48:33 +0700

    fix messages

--- application/views/messages/index.php
+++ application/views/messages/index.php
@@ -29,36 +29,48 @@
 				</ul>
 			</div>
 			<div class="col-md-9" id="message-room">
-					<div class="header-text-2 text-center"> Percakapan Dengan Kevin </div>
-					<div class="manage-books-container">
-					<?php for($i=0;$i<4;$i++) { ?>
-						
-						<div class="col-md-12 chatbox left-side">
-							<div class="chat-user">
-								<span class="chat-username ">The Other Kevin</span>
-								<img class="img-circle" src="https://scontent-sin6-1.cdninstagram.com/t51.2885-19/11849231_1115027588527403_1040877616_a.jpg" height="30" width="30">
-							</div>
-							<div class="chat-content">
-								Hola
-							</div>
+				<div class="header-text-2 text-center"> Percakapan Dengan Kevin </div>
+				<div class="manage-books-container">
+				<?php for($i=0;$i<2;$i++) { ?>
+					
+					<div class="col-md-12 chatbox left-side">
+						<div class="chat-user">
+							<img class="img-circle" src="https://scontent-sin6-1.cdninstagram.com/t51.2885-19/11849231_1115027588527403_1040877616_a.jpg" height="30" width="30">
+							<span class="chat-username ">The Other Kevin</span>
 						</div>
-						<div class="col-md-12 chatbox right-side">
-							<div class="chat-user">
-								<span class="chat-username ">The Other Kevin</span>
-								<img class="img-circle" src="https://scontent-sin6-1.cdninstagram.com/t51.2885-19/11849231_1115027588527403_1040877616_a.jpg" height="30" width="30">
-							</div>
-							<div class="chat-content">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non est quis diam egestas tempor sit amet et odio. Proin ac tortor eu augue vestibulum efficitur non eu velit. Aliquam lacinia sodales quam. Nam eu bibendum velit. Morbi ac est quis tellus commodo volutpat sed et velit. Pellentesque at nulla at neque facilisis sodales ut eget leo. Nunc id tellus venenatis, semper nulla ac, eleifend diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce aliquam sit amet ante nec porttitor. Donec porta erat non tellus aliquam mollis sed elementum tellus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-							</div>
+						<div class="chat-content">
+							Hola
 						</div>
-					<?php } ?>
 					</div>
+					<div class="col-md-12 chatbox right-side">
+						<div class="chat-user">
+							<span class="chat-username ">The Other Kevin</span>
+							<img class="img-circle" src="https://scontent-sin6-1.cdninstagram.com/t51.2885-19/11849231_1115027588527403_1040877616_a.jpg" height="30" width="30">
+						</div>
+						<div class="chat-content">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non est quis diam egestas tempor sit amet et odio. Proin ac tortor eu augue vestibulum efficitur non eu velit. Aliquam lacinia sodales quam. Nam eu bibendum velit. Morbi ac est quis tellus commodo volutpat sed et velit. Pellentesque at nulla at neque facilisis sodales ut eget leo. Nunc id tellus venenatis, semper nulla ac, eleifend diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce aliquam sit amet ante nec porttitor. Donec porta erat non tellus aliquam mollis sed elementum tellus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+						</div>
 					</div>
+				<?php } ?>
+				
+					<div class="col-md-12 chatbox">
+						<div class="chat-user">
+							<img class="img-circle" src="https://scontent-sin6-1.cdninstagram.com/t51.2885-19/11849231_1115027588527403_1040877616_a.jpg" height="30" width="30">
+							<span class="chat-username ">The Other Kevin</span>
+						</div>
+						<div class="">
+							<form action="<?php echo base_url();?>auth/do_register" method="POST">
+								<textarea class="form-control" rows="3" id="comment" placeholder="Deskripsi"></textarea>
+								<button type="submit"  class="btn btn-md btn-primary pull-right">Submit</button>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>				
-	</div>
-	
+		</div>
+	</div>				
 </div>
+	
 	
 	<?php echo $footer; ?>
 	
