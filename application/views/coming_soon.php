20726673dff2573b52989d8c0e2fa5bb7f82e4aa
DESKTOP-HTOPP7T\Kevin <kevinfachreza@yahoo.com>
2016-11-16 19:05:00 +0700

    coming-soon

---
+++ application/views/coming_soon.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo base_url()?>/assets/css/bootstrap.css">
+	<link rel='shortcut icon' href='<?php echo base_url()?>/assets/img/favicon.png' type='image/x-icon'/ >
+	<meta property="og:image" content ="<?php echo base_url()?>/assets/img/favicon.png">
+
+	<title>Dapatbuku - Coming Soon</title>
+	<style>
+	html { 
+		  background: url(<?php echo base_url()?>assets/img/bg.jpg) no-repeat center center fixed; 
+		  -webkit-background-size: cover;
+		  -moz-background-size: cover;
+		  -o-background-size: cover;
+		  background-size: cover;
+		}
+	
+	.col-md-6 img{
+		width:100%;
+	}
+	
+	.col-md-6{
+		padding-top:25vh;
+	}
+	
+	</style>
+</head>
+<body>
+	<div class="col-md-6 col-sm-8 col-xs-12 text-center col-sm-offset-2 col-xs-offset-0 col-md-offset-3">
+		<img src="<?php echo base_url()?>assets/img/comingsoon.png">
+	</div>
+</body>
+</html>
