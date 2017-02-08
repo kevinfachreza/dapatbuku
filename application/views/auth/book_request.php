0f3e2a205ef9f0649a1754ff4bd7acb7c38c9164
harisw <harrysw6@gmail.com>
2017-02-08 10:17:45 +0700

    added Login constraint Book_request

--- application/views/auth/book_request.php
+++ application/views/auth/book_request.php
@@ -50,8 +50,8 @@
 					<div class="col-md-12 header-text-2">Tidak Menemukan Buku yang Kamu Cari?</div>
 					<div class="col-md-12 text-center"><hr></div>
 					<div class="col-md-12">
-			          <form action="<?php echo base_url(); ?>/auth/do_book_request" method="post">
-					<?php if($this->session->logged_in != 1){ ?>
+			    <form action="<?php echo base_url(); ?>/auth/do_book_request" method="post">
+					<?php if($this->session->logged_in == 1){ ?>
 						<div class="row form-profile-wrapper">
 							<div class="col-md-4 col-sm-12 col-xs-12">Nama</div>
 							<div class="col-md-8 col-sm-12 col-xs-12">
@@ -64,8 +64,6 @@
 							<input name="hp" type="text"  class="form-control" placeholder="Nomor HP" style="margin-bottom:0.5em" required autofocus>
 							</div>
 						</div>
-					<?php } ?>
-
 						<div class="row form-profile-wrapper">
 							<div class="col-md-4 col-sm-12 col-xs-12">Judul</div>
 							<div class="col-md-8 col-sm-12 col-xs-12"><input name="title" type="text"  class="form-control" placeholder="Judul Buku" style="margin-bottom:0.5em" required autofocus>
@@ -79,7 +77,6 @@
        						<input name="author" type="text" class="form-control" placeholder="Pengarang"  autofocus>
 							</div>
 						</div>
-
 
 						<div class="row form-profile-wrapper"  style="margin-bottom:0.5em">
 							<div class="col-md-4 col-sm-12 col-xs-12">Butuh Buku Untuk</div>
@@ -95,6 +92,33 @@
 
 			            <button type="submit" class="btn btn-primary" value="Submit" style="margin-top:10px;float:right">Kirim</button>
 			          </form>
+					<?php }
+						else {
+						?>
+						<div class="text-center">Silahkan login terlebih dahulu</div>
+						<a data-toggle="modal" data-target="#loginModal"><button type="submit" class="btn btn-primary" value="Login" style="margin-top:10px;float:right">Login</button></a>
+						<div id="loginModal" class="modal fade">
+						    <div class="modal-dialog">
+						        <div class="modal-content">
+						            <div class="modal-header">
+						                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						                <h4 class="modal-title">Login</h4>
+						            </div>
+						            <div class="modal-body">
+						                <p>Silahkan login untuk mulai menggunakan dapatbuku</p>
+													<form  action="<?php echo base_url();?>Auth/do_login" method="POST">
+														<input name="log-user_in" type="text"  class="form-control" placeholder="Masukkan Email anda" style="margin-bottom:0.5em" required autofocus>
+														<input name="log-pass" type="password" class="form-control" placeholder="Masukkan Password anda" style="margin-bottom:1em" required  autofocus>
+														<button type="submit" style="width:100%; border:none" class="btn btn-lg btn-sch-color btn-primary">LOGIN</button>
+													</form>
+													<p style="margin-top:1em; font-size:1.2em">Belum punya akun?</p>
+													<a href="<?php echo base_url()?>auth/register" >REGISTER</a>
+						            </div>
+						        </div>
+						    </div>
+						</div>
+						<?php } ?>
+
 					</div>
 				</div>
 				</div>
