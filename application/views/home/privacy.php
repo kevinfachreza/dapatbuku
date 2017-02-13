852e9c220cfe3895de0112e3d2f305fbdd75ad27
kevinfachreza <kevinfachreza@yahoo.com>
2017-02-13 21:28:58 +0700

    new footer + ketentuan

---
+++ application/views/home/privacy.php
@@ -0,0 +1,23 @@
+
+<div class="container custom-container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2"  id="content-container">
+			<div class="about-header">
+				<h1>Privasi</h1>
+				<hr class="about-hr">
+			</div>
+			<div id="privacy" class="about-content">
+
+
+<ol>
+	
+<li>Dapatbuku melindungi segala informasi yang diberikan pengguna pada saat pendaftaran, mengakses, dan menggunakan seluruh layanan Dapatbuku</li>
+<li>Dapatbuku berhak menggunakan data dan informasi para pengguna situs demi meningkatkan mutu dan pelayanan di Dapatbuku.</li>
+<li>Dapatbuku tidak bertanggung jawab atas pertukaran data yang dilakukan sendiri di antara pengguna situs</li>
+
+</ol>
+
+			</div>
+		</div>
+	</div>
+</div>
---
+++ application/views/home/transaksi.php
@@ -0,0 +1,142 @@
+
+<div class="container custom-container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2"  id="content-container">
+			<div class="about-header">
+				<h1>Cara Transaksi</h1>
+				<hr class="about-hr">
+			</div>
+			<div id="transaksi" class="about-content">
+
+
+<h4>Cara Beli di Dapatbuku (Pembeli)</h4>
+<ol>
+	<li>Cari buku<br>
+	Kamu bisa mencari buku melalui fitur "Search" atau melihat pada kategori yang telah disediakan</li>
+
+	<li>Klik Buku<br>
+	Disini kamu bisa melihat informasi mengenai buku, mulai dari halaman, hingga review buku, dan dimana bisa mendapatkan buku tersebut
+	</li>
+
+	<li>Klik Penjual<br>
+	Jika ada pengguna yang menjual buku tersebut, kamu bisa mengklik untuk melihat halaman iklan yang dia tampilkan.
+	</li>
+
+	<li>Hubungi Penjual<br>
+	Disini kamu bisa menetapkan, bagaimana kamu ingin mendapatkan buku tersebut. Bisa melalui pengiriman ataupun kamu bertemu dengan penjual.
+	</li>
+
+	<li>Dapat Bukunya<br>
+	Setelah deal dengan penjual, kamu akan mendapatkan buku yang kamu inginkan.
+	</li>
+
+	<li>Baca Bukunya & kasih review<br>
+	Baca bukunya sampe habis, jangan lupa kasih review tentang buku tersebut di Dapatbuku, agar yang lain juga tertarik membaca buku yang kamu baca
+	</li>
+
+</ol>
+
+<hr>
+
+Cara Jual di Dapatbuku (Penjual)
+1. Klik jual buku
+	Isi form yang terdapat pada menu jual buku, jika kamu ingin menjual bukumu silahkan isi kolom harga jual sesuai harga yang kamu inginkan
+
+2. Validasi
+	Kami akan memvalidasikan iklan yang kamu iklankan dalam waktu 1x24 jam. Kami berhak menolak iklanmu jika kamu melanggar aturan yang berlaku di Dapatbuku
+
+3. Dapat Pembeli
+	Jika pembeli ada yang menghubungi kamu, silahkan jual buku kamu sesuai kesepakatan kamu dengan si pembeli. 
+
+4. Buku Terjual
+	Setelah buku terjual, kamu bisa mengedit stok yang terdapat pada menu 'Edit Buku' atau hapus jika memang kamu ingin menghapus iklan tersebut.
+
+
+Cara Rental di Dapabuku (Perental)
+1. Cari buku
+	Kamu bisa mencari buku melalui fitur "Search" atau melihat pada kategori yang telah kami sediakan
+
+2. Klik Buku
+	Disini kamu bisa melihat informasi mengenai buku, mulai dari halaman, hingga review buku, dan dimana bisa mendapatkan buku tersebut
+
+3. Klik Penjual
+	Jika ada pengguna yang merentalkan buku tersebut, kamu bisa mengklik untuk melihat halaman iklan yang dia tampilkan.
+
+4. Hubungi Penjual
+	Disini kamu bisa menetapkan, bagaimana kamu ingin mendapatkan buku tersebut. Bisa melalui pengiriman ataupun kamu bertemu dengan penjual. Dan durasi peminjaman
+
+5. Dapat Bukunya
+	Setelah deal dengan penjual, bayar sesuai harga buku + harga sewa buku sesuai dengan durasi. Kenapa kok bayar harga buku juga? Karena sebagai jaminan, jika terjadi kerusakan atau kehilangan buku.
+
+6. Baca Bukunya & kasih review
+	Baca bukunya sampe habis, jangan lupa kasih review tentang buku tersebut di Dapatbuku, agar yang lain juga tertarik membaca buku yang kamu baca
+
+7. Kembalikan buku
+	Setelah kamu kembalikan buku, maka penjual wajib mengembalikan harga. Jika kamu terlambat, maka kamu akan membayar denda seharga sewa buku.
+
+
+Cara Rental di Dapatbuku (Penyedia)
+1. Klik jual buku
+	Isi form yang terdapat pada menu jual buku, jika kamu ingin merentalkan bukumu silahkan isi kolom harga rental sesuai harga yang kamu inginkan.
+	Ingat, harga rental adalah harga perminggu.
+
+2. Validasi
+	Kami akan memvalidasikan iklan yang kamu iklankan dalam waktu 1x24 jam. Kami berhak menolak iklanmu jika kamu melanggar aturan yang berlaku di Dapatbuku
+
+3. Dapat Pembeli
+	Jika pembeli ada yang menghubungi kamu, silahkan rentalkan buku kamu sesuai kesepakatan kamu dengan si pembeli. 
+	Sebagai jaminan, kami sarankan pembeli membayar seharga harga buku + harga total rental. Harga buku sebagai jaminan, jika bukumu rusak atau hilang.
+
+4. Buku Tersewa
+	Saat proses menyewa, jangan lupa untuk mengingatkan si penyewa soal waktu pengembalian buku.
+
+5. Buku dikembalikan
+	Ketika buku kembali, kamu cek dulu apakah buku masih dalam keadaan baik atau sudah dalam keadaan yang tidak karuan.
+	Kamu berhak memberikan denda kepada perental jika kamu merasa buku yang dirental terjadi kerusakan.
+	Setelah itu, kamu wajib mengembalikan uang jaminan yang diberikan perental kepada kamu.
+ 
+Cara Barter di Dapatbuku
+1. Cari buku
+	Kamu bisa mencari buku melalui fitur "Search" atau melihat pada kategori yang telah kami sediakan
+
+2. Klik Buku
+	Disini kamu bisa melihat informasi mengenai buku, mulai dari halaman, hingga review buku, dan dimana bisa mendapatkan buku tersebut
+
+3. Klik Penjual
+	Jika ada pengguna yang mau menukarkan buku tersebut, kamu bisa mengklik untuk melihat halaman iklan yang dia tampilkan.
+
+4. Hubungi Penjual
+	Disini kamu akan menawarkan buku apa yang ingin kamu tukarkan dengan buku milik si Penjual. Jika dia mau menukarkan bukunya dengan bukumu maka kamu
+	bisa menetapkan, bagaimana kamu ingin mendapatkan buku tersebut. Bisa melalui pengiriman ataupun kamu bertemu dengan penjual. 
+	Kami sangat menyarankan untuk bertemu dengan penjual untuk transaksi barter. Supaya kedua belah pihak dapat melihat kondisi buku dan tidak terjadi kesalahan.
+
+5. Dapat Bukunya
+	Setelah deal dengan penjual kamu bisa dapetin buku yang kamu inginkan.
+
+6. Baca Bukunya & kasih review
+	Baca bukunya sampe habis, jangan lupa kasih review tentang buku tersebut di Dapatbuku, agar yang lain juga tertarik membaca buku yang kamu baca
+
+Cara Barter di Dapatbuku (Penyedia)
+1. Klik jual buku
+	Isi form yang terdapat pada menu jual buku, jika kamu bersedia mem barterkan bukumu silahkan centang kolom barter sesuai harga yang kamu inginkan.
+
+2. Validasi
+	Kami akan memvalidasikan iklan yang kamu iklankan dalam waktu 1x24 jam. Kami berhak menolak iklanmu jika kamu melanggar aturan yang berlaku di Dapatbuku
+
+3. Dapat Pembeli
+	Jika pembeli ada yang menghubungi kamu, jangan lupa tanyakan buku apa yang dimiliki dia. Jika sudah deal kami sangat sarankan kamu bertemu dengan
+	pengguna yang ingin menukarkan bukunya dengan kamu, agar tidak terjadi penipuan.
+
+4. Bukunya telah tertukar
+	Setelah deal dengan penukar kamu bisa dapetin buku yang kamu sepakati.
+
+5. Baca Bukunya & kasih review
+	Baca bukunya sampe habis, jangan lupa kasih review tentang buku tersebut di Dapatbuku, agar yang lain juga tertarik membaca buku yang kamu baca
+
+
+
+
+			</div>
+		</div>
+	</div>
+</div>
